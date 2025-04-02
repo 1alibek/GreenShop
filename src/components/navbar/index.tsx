5b1c3fc271345a4ad7bb0d2627e5d6ca4e49005e
1alibek <alibekgithub@gmail.com>
2025-04-02 17:37:01 +0500

    done::

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -18,16 +18,7 @@ import { FaXmark } from "react-icons/fa6";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [placement] = useState<DrawerProps["placement"]>("left");
-const {data}=useReduxSelector(state=>state.shopSlice)
-  const showDrawer = () => {
-    setOpen(true);
-  };
-  const showDrawer2 = () => {
-    setOpen(false);
-  };
-  const onClose = () => {
-    setOpen(false);
-  };
+  const { data } = useReduxSelector(state => state.shopSlice);
 
   const dispatch = useReduxDispatch();
   const { pathname } = useLocation();
@@ -36,6 +27,12 @@ const {data}=useReduxSelector(state=>state.shopSlice)
   const user = getCookie("user");
 
   const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+    setOpen(false);
+  };
+
   return (
     <div className="container2 flex items-center justify-between border-b border-primary max-[600px]:py-3">
       <Link to={"/"}>
@@ -44,45 +41,43 @@ const {data}=useReduxSelector(state=>state.shopSlice)
       </Link>
       <div className="flex items-center gap-5 py-5 max-[600px]:hidden">
         <Link to={"/"}>
-        <h3
-          className={`cursor-pointer  relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full ${
-            pathname === "/" && "text-primary text-[16px]"
-          }`}
+          <h3
+            className={`cursor-pointer relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full ${
+              pathname === "/" && "text-primary text-[16px]"
+            }`}
           >
-          Home
-        </h3>
-          </Link>
+            Home
+          </h3>
+        </Link>
         <Link to={"/blogs"}>
-        <h3
-          className={`cursor-pointer  relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full ${
-            pathname === "/blogs" && "text-primary text-[16px]"
-          }`}
+          <h3
+            className={`cursor-pointer relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full ${
+              pathname === "/blogs" && "text-primary text-[16px]"
+            }`}
           >
-          Blogs
-        </h3>
-          </Link>
+            Blogs
+          </h3>
+        </Link>
       </div>
       <div className="flex items-center gap-5">
         <BiSearch className="w-[25px] h-[25px] max-[390px]:w-[20px]" />
         <Badge count={data?.length}>
-
-        <button onClick={() => navigate("/products-shop")}>
-          <img
-            className="w-[25px] h-[25px] max-[390px]:w-[20px]"
-            src={shop}
-            alt=""
+          <button onClick={() => navigate("/products-shop")}>
+            <img
+              className="w-[25px] h-[25px] max-[390px]:w-[20px]"
+              src={shop}
+              alt=""
             />
-        </button>
-            </Badge>
+          </button>
+        </Badge>
         <button
-        onClick={() => {
-          if (isAuthorization) {
-            navigate("/profile");
-          } else {
-            dispatch(setModalAuthVisibility());
-          }
-        }}
-        
+          onClick={() => {
+            if (isAuthorization) {
+              navigate("/profile");
+            } else {
+              dispatch(setModalAuthVisibility());
+            }
+          }}
           className="bg-primary text-white font-medium flex items-center gap-2 px-3 py-2 rounded-md border border-primary transition-all duration-300 hover:bg-transparent hover:text-primary max-[600px]:hidden"
         >
           {isAuthorization ? user.name : <>Login</>}
@@ -95,8 +90,8 @@ const {data}=useReduxSelector(state=>state.shopSlice)
           />
         </button>
         <button
-          onClick={showDrawer}
-          className="bg-primary text-white font-medium  gap-2 px-3 py-2 rounded-md border border-primary transition-all duration-300 hover:bg-transparent hover:text-primary hidden max-[600px]:block"
+          onClick={() => setOpen(true)}
+          className="bg-primary text-white font-medium gap-2 px-3 py-2 rounded-md border border-primary transition-all duration-300 hover:bg-transparent hover:text-primary hidden max-[600px]:block"
         >
           <CgMenuRightAlt />
         </button>
@@ -104,30 +99,29 @@ const {data}=useReduxSelector(state=>state.shopSlice)
       <Drawer
         placement={placement}
         closable={false}
-        onClose={onClose}
+        onClose={() => setOpen(false)}
         open={open}
         key={placement}
       >
         <div className="flex items-center justify-between">
           <img src={logo} alt="" />
           <button
-            onClick={showDrawer2}
-            className="bg-primary text-white font-medium  gap-2 px-3 py-2 rounded-md border border-primary transition-all duration-300 hover:bg-transparent hover:text-primary hidden max-[600px]:block"
+            onClick={() => setOpen(false)}
+            className="bg-primary text-white font-medium gap-2 px-3 py-2 rounded-md border border-primary transition-all duration-300 hover:bg-transparent hover:text-primary hidden max-[600px]:block"
           >
             <FaXmark />
           </button>
         </div>
         <div className="flex items-center gap-5 py-5 flex-col">
           <h3
-            className="cursor-pointer  relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full 
-          
-          "
+            className="cursor-pointer relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full"
+            onClick={() => handleNavigate("/")}
           >
             Home
           </h3>
           <h3
-            className="cursor-pointer  relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full 
-          "
+            className="cursor-pointer relative pb-1 after:content-[''] font-medium after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-green-600 after:transition-all after:duration-300 hover:after:w-full"
+            onClick={() => handleNavigate("/blogs")}
           >
             Blogs
           </h3>
